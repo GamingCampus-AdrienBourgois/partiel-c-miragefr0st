6a982a4bf98ebca70b5e96daa6e0c436dc85a3f0
mrasolojaona <mrasolojaona@gaming.tech>
2024-01-19 14:07:03 +0100

    Exercice3 v2

--- StudentLibrary/Solution3.cpp
+++ StudentLibrary/Solution3.cpp
@@ -1,11 +1,12 @@
+// Solution3.cpp
+
 #include "Solution3.h"
 
 // Don't forget to enable the exercise in the StudentConfiguration.h file !
 #include "StudentConfiguration.h"
-#ifdef COMPILE_EXERCICE_3
-
 #include <algorithm>
 #include <stdexcept>
+#ifdef COMPILE_EXERCICE_3
 
 void Solution3::SetWords(const std::vector<std::string>& _words)
 {
@@ -14,24 +15,21 @@ void Solution3::SetWords(const std::vector<std::string>& _words)
 
 void Solution3::SortWords()
 {
-    // Check if the vector is empty before attempting to sort
     if (words.empty())
     {
         throw std::logic_error("Cannot sort an empty vector of words.");
     }
 
-    // Bubble sort for simplicity
+
     for (size_t i = 0; i < words.size() - 1; ++i)
     {
         for (size_t j = 0; j < words.size() - i - 1; ++j)
         {
-            // Convert both strings to lowercase for case-insensitive comparison
             std::string lowerA = words[j];
             std::string lowerB = words[j + 1];
             std::transform(lowerA.begin(), lowerA.end(), lowerA.begin(), ::tolower);
             std::transform(lowerB.begin(), lowerB.end(), lowerB.begin(), ::tolower);
 
-            // Swap if they are in the wrong order
             if (lowerA > lowerB)
             {
                 std::swap(words[j], words[j + 1]);
@@ -40,10 +38,9 @@ void Solution3::SortWords()
     }
 }
 
-const std::vector<std::string>& Solution3::GetSortedWords() const
+std::vector<std::string> Solution3::GetSortedWords() const
 {
     return words;
 }
 
 #endif
-
