fa24d17dd3f679b46bb0015c5f8f00602ed6fb4f
mrasolojaona <mrasolojaona@gaming.tech>
2024-01-19 13:28:22 +0100

    Exercice1

--- StudentLibrary/Solution1.cpp
+++ StudentLibrary/Solution1.cpp
@@ -6,7 +6,41 @@
 
 float Solution1::ConvertTemperature(float _value, TemperatureUnits _from, TemperatureUnits _to)
 {
-	return -1.0f;
+    switch (_from)
+    {
+    case TemperatureUnits::CELSIUS:
+        switch (_to)
+        {
+        case TemperatureUnits::FAHRENHEIT:
+            return (_value * 9.0f / 5.0f) + 32.0f;
+        case TemperatureUnits::KELVIN:
+            return _value + 273.0f;
+        default:
+            return _value; 
+        }
+    case TemperatureUnits::FAHRENHEIT:
+        switch (_to)
+        {
+        case TemperatureUnits::CELSIUS:
+            return (_value - 32.0f) * 5.0f / 9.0f;
+        case TemperatureUnits::KELVIN:
+            return (_value - 32.0f) * 5.0f / 9.0f + 273.0f;
+        default:
+            return _value; 
+        }
+    case TemperatureUnits::KELVIN:
+        switch (_to)
+        {
+        case TemperatureUnits::CELSIUS:
+            return _value - 273.0f;
+        case TemperatureUnits::FAHRENHEIT:
+            return (_value - 273.0f) * 9.0f / 5.0f + 32.0f;
+        default:
+            return _value; 
+        }
+    default:
+        return _value; 
+    }
 }
 
 #endif
