245dac48322c0a274850631d471400dcec3e7392
mrasolojaona <mrasolojaona@gaming.tech>
2024-01-19 14:21:49 +0100

    Exercice2 v1

--- StudentLibrary/Solution2.cpp
+++ StudentLibrary/Solution2.cpp
@@ -1,6 +1,7 @@
 #include "Solution2.h"
-
-#include <ostream>
+#include <fstream>
+#include <iostream>
+#include <sstream>
 
 // Don't forget to enable the exercise in the SudentConfiguration.h file !
 #include "StudentConfiguration.h"
@@ -8,7 +9,40 @@
 
 float Solution2::GetBalance(const std::string& accountName)
 {
-	return -1.0f;
+    std::string filePath = "BankAccount/" + accountName + ".txt";
+    std::ifstream file(filePath);
+
+    if (!file.is_open())
+    {
+        throw std::runtime_error("File not found");
+    }
+
+    float balance = 0.0f;
+    std::string line;
+
+    while (std::getline(file, line))
+    {
+        std::istringstream lineStream(line);
+        std::string operation;
+        float amount;
+
+        if (lineStream >> operation >> amount)
+        {
+            if (operation == "DEPOSIT") {
+                balance += amount;
+            }
+            else if (operation == "WITHDRAW") {
+                balance -= amount;
+            }
+            else {
+                throw std::exception("Invalid operation");
+            }
+        }
+    }
+
+    file.close();
+
+    return balance;
 }
 
 #endif
